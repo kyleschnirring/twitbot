7a88c36cce871e9f7482b09ce403c96e700ea877
kyleschnirring <kyleschnirring@icloud.com>
2015-09-10 18:03:34 -0700

    bot saves tweets then tweets random nodejs tweet

--- twitbot.js
+++ twitbot.js
@@ -5,3 +5,29 @@ var Bot = new Twit({
 
 });
 
+var stream = Bot.stream('statuses/filter', { track: '#nodejs', language: 'en' });
+var ids = [];
+
+stream.on('tweet', function (tweet) {
+  console.log(tweet['id_str']);
+  ids.push(tweet['id_str']);
+  console.log(ids.length);
+});
+
+setInterval(function(){
+  ids.map(String);
+  var randomTweet = Math.floor(Math.random() * ids.length) + 1;
+  var tweets = ids[randomTweet];
+  console.log(tweets);
+
+
+  Bot.post('statuses/retweet/:id', { id: tweets }, function (err, data, response) {
+  console.log(data);
+  });
+
+  while(ids.length > 0) {
+    ids.pop();
+  }
+
+
+}, 18000000);
