833f16bdae85a5717504457aac5a1d5e92076589
kyleschnirring <kyleschnirring@icloud.com>
2015-09-24 18:44:46 -0700

    added bot functions to get followers/friends

--- twitbot.js
+++ twitbot.js
@@ -5,27 +5,52 @@ var Bot = new Twit({
 
 });
 
-var stream = Bot.stream('statuses/filter', { track: '#nodejs', language: 'en' });
-var ids = [];
+var streamIt = setInterval(function () {
+  var stream = Bot.stream('statuses/filter', { track: '#nodejs', language: 'en' });
+  var ids = [];
+  var followers = [];
+  var friends = [];
 
-stream.on('tweet', function (tweet) {
-  console.log(tweet['id_str']);
-  ids.push(tweet['id_str']);
-  console.log(ids.length);
-});
-
-setInterval(function(){
-  ids.map(String);
-  var randomTweet = Math.floor(Math.random() * ids.length) + 1;
-  var tweets = ids[randomTweet];
-  console.log(tweets);
-
-  Bot.post('statuses/retweet/:id', { id: tweets }, function (err, data, response) {
-  console.log(data);
+  Bot.get('followers/ids', { screen_name: 'Kschnirring' },  function (error, data, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      followers = data;
+      console.log(followers);
+    }
   });
 
-  while(ids.length > 0) {
-    ids.pop();
-  }
+  Bot.get('friends/ids', { screen_name: 'Kschnirring' },  function (error, data, response) {
+    if (error){
+      console.log(error);
+    } else {
+      friends = data;
+      console.log(friends);
+    }
+  });
 
-}, 18000000);
+  stream.on('tweet', function (tweet) {
+    console.log(tweet['id_str']);
+    ids.push(tweet['id_str']);
+    console.log(ids.length);
+
+  });
+
+  setInterval(function () {
+    ids.map(String);
+    var randomTweet = Math.floor(Math.random() * ids.length) + 1;
+    var tweets = ids[randomTweet];
+    console.log(tweets);
+
+    Bot.post('statuses/retweet/:id', { id: tweets }, function (error, data, response) {
+      console.log(data);
+    });
+
+    while (ids.length > 0) {
+      ids.pop();
+    }
+    clearInterval(streamIt);
+  }, 144000);
+}, 108000);//3 hours
+
+
