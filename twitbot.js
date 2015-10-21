4d5e52e82614f824bbe6cfea444c5cf1a4172932
kyleschnirring <kyleschnirring@icloud.com>
2015-10-21 12:32:56 -0700

    removed friends/followers

--- twitbot.js
+++ twitbot.js
@@ -2,55 +2,41 @@ var Twit = require("twit");
 
 // Include your access information below
 var Bot = new Twit({
+   consumer_key:         'LEU21rEE7t8AoT6FrgATpgUi7'
+  , consumer_secret:      '7ghVxQnxqcyzsOn0XLTdXoi4qYHBh8MFEsTftzpAzyt4lZG38V'
+  , access_token:         '159317043-fMrDMCbVsFy2LmVf9DAUJC6y5RaxnIPHdM7cK3je'
+  , access_token_secret:  'RCE4szrLjOiJ0uTKbbqPUZYNXytfnvIEgsRNEGicPafRX'
 
 });
 
 var streamIt = setInterval(function () {
   var stream = Bot.stream('statuses/filter', { track: '#nodejs', language: 'en' });
-  var ids = [];
-  var followers = [];
-  var friends = [];
-
-  Bot.get('followers/ids', { screen_name: 'Kschnirring' },  function (error, data, response) {
-    if (error) {
-      console.log(error);
-    } else {
-      followers = data;
-      console.log(followers);
-    }
-  });
-
-  Bot.get('friends/ids', { screen_name: 'Kschnirring' },  function (error, data, response) {
-    if (error){
-      console.log(error);
-    } else {
-      friends = data;
-      console.log(friends);
-    }
-  });
+  var tweetids = [];
 
   stream.on('tweet', function (tweet) {
     console.log(tweet['id_str']);
-    ids.push(tweet['id_str']);
-    console.log(ids.length);
+    tweetids.push(tweet['id_str']);
+    console.log(tweetids.length);
 
   });
 
   setInterval(function () {
-    ids.map(String);
-    var randomTweet = Math.floor(Math.random() * ids.length) + 1;
-    var tweets = ids[randomTweet];
+    tweetids.map(String);
+    var randomTweet = Math.floor(Math.random() * tweetids.length) + 1;
+    var tweets = tweetids[randomTweet];
+    var counter = 0;
+    var friendAndFollower = [];
     console.log(tweets);
 
     Bot.post('statuses/retweet/:id', { id: tweets }, function (error, data, response) {
       console.log(data);
     });
 
-    while (ids.length > 0) {
-      ids.pop();
+    while (tweetids.length > 0) {
+      tweetids.pop();
     }
     clearInterval(streamIt);
   }, 144000);
-}, 108000);//3 hours
+}, 108000);
 
 
